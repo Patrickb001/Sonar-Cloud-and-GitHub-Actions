02013896673f205f4db7a5e1e164cd5112741f69
Patrick Borgella <patrickborgella1@gmail.com>
2023-05-31 09:50:16 -0500

    updates build.yml file and create sonarproject properties file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const Handlebars = require('handlebars');
-const expressHandlebars = require('express-handlebars');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const express = require("express");
+const Handlebars = require("handlebars");
+const expressHandlebars = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 
-const {sequelize} = require('./db');
-const {Sauce} = require('./models');
-const seed = require('./seed')
+const { sequelize } = require("./db");
+const { Sauce } = require("./models");
+const seed = require("./seed");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,27 +15,28 @@ const app = express();
 
 // setup our templating engine
 const handlebars = expressHandlebars({
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
-})
-app.engine('handlebars', handlebars);
-app.set('view engine', 'handlebars');
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+});
+app.engine("handlebars", handlebars);
+app.set("view engine", "handlebars");
 
 // serve static assets from the public/ folder
-app.use(express.static('public'));
+// New comment for new push
+app.use(express.static("public"));
 
 seed();
 
-app.get('/sauces', async (req, res) => {
-    const sauces= await Sauce.findAll()
-    res.render('sauces', {sauces}); //points to sauces handlebar
-})
+app.get("/sauces", async (req, res) => {
+  const sauces = await Sauce.findAll();
+  res.render("sauces", { sauces }); //points to sauces handlebar
+});
 
-app.get('/sauces/:id', async (req, res) => {
-    const sauce = await Sauce.findByPk(req.params.id)
-    res.render('sauce', {sauce}); 
-})
+app.get("/sauces/:id", async (req, res) => {
+  const sauce = await Sauce.findByPk(req.params.id);
+  res.render("sauce", { sauce });
+});
 
 app.listen(PORT, () => {
-    sequelize.sync({force: true});
-    console.log(`Your server is running on http://localhost:${PORT}`);
-})
+  sequelize.sync({ force: true });
+  console.log(`Your server is running on http://localhost:${PORT}`);
+});
